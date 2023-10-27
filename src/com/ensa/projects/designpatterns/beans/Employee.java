fe9bd7bdeb8f8923834cde00b2d245224854eacd
sha3loul <zarhloulyasser@gmail.com>
2023-10-27 01:31:51 +0100

    added constructor for Employee

--- src/com/ensa/projects/designpatterns/beans/Employee.java
+++ src/com/ensa/projects/designpatterns/beans/Employee.java
@@ -1,9 +1,13 @@
 package com.ensa.projects.designpatterns.beans;
 
 public class Employee {
-
     private int id;
     private String nom;
+
+    public Employee(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
 
     // constructors, getters, setters
 }
